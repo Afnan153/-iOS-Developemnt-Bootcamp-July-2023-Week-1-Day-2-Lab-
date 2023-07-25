b2a2e393dc6ec446a85c98e617cd8d52a3e53d8f
Afnan153 <140311940+Afnan153@users.noreply.github.com>
2023-07-25 18:26:39 +0300

    Add files via upload

---
+++ Contents.swift
@@ -0,0 +1,31 @@
+//task 1 declir a variable with value
+var  scor : Int
+scor = 10
+print(scor)
+
+//task 2 controll flow
+if scor > 9 {
+ print ("pass")
+}
+   else {
+ print ("Fail")
+    
+}
+
+//task 3 loop
+var Array = ["Ahmad","Manal","Afnan","sarah","Amal"]
+print ("Array elements are:")
+for element in Array{
+    print(element)
+    
+}
+//task 4 dictionaries
+let fruit = [1:"apple",2:"orange",3:"banana"]
+for items in fruit{
+    print(items)
+}
+
+//task 5 string intrepolation
+let PersonName : String
+var Name = "Afnan"
+print("welcom\"(Afnan)")
